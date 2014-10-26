1a8b0f71b581085521cbcf4cee98074b54f00b0e
walshification <walshification@gmail.com>
2014-10-26 14:39:29 -0500

    postgres error fix to db

--- db/migrate/20141022022054_change_photo_i_dand_option_i_dto_integer.rb
+++ db/migrate/20141022022054_change_photo_i_dand_option_i_dto_integer.rb
@@ -1,6 +1,6 @@
 class ChangePhotoIDandOptionIDtoInteger < ActiveRecord::Migration
   def change
-    change_column :photos, :product_id, :integer
-    change_column :options, :product_id, :integer
+    change_column :photos, :product_id, 'integer USING CAST("column_to_change" AS integer)'
+    change_column :options, :product_id, 'integer USING CAST("column_to_change" AS integer)'
   end
 end
