e8c0247813b7552fa4cccc02e1aae8e172d8bde1
walshification <walshification@gmail.com>
2014-10-26 15:03:40 -0500

    heroku db migrate, Take 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,6 @@ Order.create!([
   {user_id: 1, product_id: 1, quantity: 2, total: "0.99", confirmation_number: nil},
   {user_id: 1, product_id: 1, quantity: 2, total: "0.99", confirmation_number: nil},
   {user_id: 1, product_id: 1, quantity: 5, total: "0.99", confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 5, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 5, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 5, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 3, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 3, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 3, total: "2.97", confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 5, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 5, total: "5.45", confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 3, total: nil, confirmation_number: nil},
-  {user_id: 1, product_id: 1, quantity: 3, total: "3.27", confirmation_number: 15},
   {user_id: 1, product_id: 1, quantity: 3, total: "3.27", confirmation_number: 16},
   {user_id: 1, product_id: 5, quantity: 4, total: "4.36", confirmation_number: 17},
   {user_id: 1, product_id: 5, quantity: 4, total: "4.36", confirmation_number: 18},
@@ -36,10 +26,7 @@ Product.create!([
   {name: "whatsit", description: "A power tool for every occasion.", price: "19.99", photo: "http://harrowsmithalmanac.com/wp-content/uploads/2012/10/whatsit-2009-614x380.jpg", vendor_id: 1},
   {name: "whosit", description: "Just the person that you need.", price: "49.99", photo: "http://yourhandymanyork.yolasite.com/resources/handyman%204.jpg", vendor_id: 1},
   {name: "Howsit", description: "The last How-To guide you'll ever need.", price: "59.99", photo: "http://www.mosaik.sg/uploads/1/6/6/0/16606024/910505_orig.jpg", vendor_id: 2},
-  {name: "Howitzer", description: "A very big gun.", price: "19.99", photo: "http://visual.merriam-webster.com/images/society/weapons/modern-howitzer.jpg", vendor_id: 2},
-  {name: "widget", description: "A sham!", price: "9.0", photo: "http://icons.iconarchive.com/icons/untergunter/leaf-mimes/256/opera-widget-icon.png", vendor_id: nil},
-  {name: "whatsit", description: "A power tool for every occasion.", price: "19.99", photo: "http://harrowsmithalmanac.com/wp-content/uploads/2012/10/whatsit-2009-614x380.jpg", vendor_id: nil},
-  {name: "whosit", description: "Just the person that you need.", price: "49.99", photo: "http://yourhandymanyork.yolasite.com/resources/handyman%204.jpg", vendor_id: nil}
+  {name: "Howitzer", description: "A very big gun.", price: "19.99", photo: "http://visual.merriam-webster.com/images/society/weapons/modern-howitzer.jpg", vendor_id: 2}
 ])
 Vendor.create!([
   {name: "WhatsItDoers", email: "what@gmail.com", phone_number: "123-456-7890"},
